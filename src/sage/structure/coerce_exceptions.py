cefa814dcb081ed0c197800bb190c5ec5981cc82
Frédéric Chapoton <chapoton@math.univ-lyon1.fr>
2017-11-12 16:12:23 +0100

    changing CoercionException

--- src/sage/structure/coerce_exceptions.py
+++ src/sage/structure/coerce_exceptions.py
@@ -9,13 +9,13 @@
 ###############################################################################
 
 
-class CoercionException(TypeError):
+class CoercionException(TypeError, BaseException):
     """
     This is the baseclass of exceptions that the coercion model raises
     when trying to discover coercions. We don't use standard Python
     exceptions to avoid inadvertently catching and suppressing real errors.
 
-    Usually one raises this to indicate the attempted action isn't
+    Usually one raises this to indicate the attempted action is not
     implemented/appropriate, but if there are other things to try not
     to immediately abort to the user.
     """
